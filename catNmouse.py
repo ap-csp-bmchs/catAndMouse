2a8d48cbdcedf778bc90da7d5e5d8010d94625d0
unknown <David Gilpin@DESKTOP-CFVS20P.bmchs.org>
2019-11-01 15:26:58 -0500

    added left and right movement to cat

--- catNmouse.py
+++ catNmouse.py
@@ -4,8 +4,10 @@ pygame.init()
 display_width = 1000
 display_height = 800
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-
 pygame.display.set_caption('Cat N Mouse')
+clock = pygame.time.Clock()
+
+catImg = pygame.image.load('cat.png')
 
 red = (255, 0, 0)
 green = (0, 255, 0)
@@ -13,15 +15,38 @@ blue = (0, 0, 255)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
+def cat(x,y):
+    gameDisplay.blit(catImg, (x,y))
+
+x = (display_width * 0.45)
+y = (display_height * 0.45)
+
+x_change = 0
+
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
 
 
-while True:
-    ev = pygame.event.poll()
-    if ev.type == pygame.QUIT:
-        break
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
 
-    gameDisplay.fill(white)
+        x += x_change   
+
+    gameDisplay.fill(black)
+    cat(x,y)
     pygame.display.flip()
+    clock.tick(60)
         
 pygame.quit()
             
