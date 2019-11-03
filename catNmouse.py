b1f7ad4d219d4ad3f0b5131cf7e9d9a53e7eba97
David Gilpin <dgilpin@bmchs.org>
2019-11-03 09:48:12 -0600

    added boundary restrictions to cat movement

--- catNmouse.py
+++ catNmouse.py
@@ -8,9 +8,6 @@ clock = pygame.time.Clock()
 
 pygame.display.set_caption('Cat N Mouse')
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
@@ -23,22 +20,31 @@ x = (display_width * 0.45)
 y = (display_height * 0.45)
 x_change = 0
 y_change = 0
+cat_width = 131
+cat_height = 75
+new_x = 0
+new_y = 0
 
-
+#game loop
 game_over = False
 while game_over == False:
+    
+#set up quit when user clicks the 'x'
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
+#set up key press definitions
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x_change = -5
+                    
             elif event.key == pygame.K_RIGHT:
                 x_change = 5
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_change = 0
+                x += x_change
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
@@ -49,11 +55,20 @@ while game_over == False:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 y_change = 0
 
-    x += x_change
-    y += y_change
+#set up x & y change to stay within window boundaries
+    new_x = x + x_change
+    if new_x >=0 and new_x <= display_width - cat_width:
+        x += x_change
+
+    new_y = y + y_change
+    if new_y >= 0 and new_y <= display_height - cat_height:
+        y += y_change
+
+#draw the frame
     gameDisplay.fill(white)
     cat(x,y)
     pygame.display.flip()
+#set up fps
     clock.tick(60)
 
 pygame.quit()
