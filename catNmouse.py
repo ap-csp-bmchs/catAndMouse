b468392d4d5432bf33e8fc9e7c5897344b04eb17
David Gilpin <dgilpin@bmchs.org>
2019-11-12 17:59:14 -0600

    finally got the image collision figured out!

--- catNmouse.py
+++ catNmouse.py
@@ -12,6 +12,8 @@ clock = pygame.time.Clock()
 catImg = pygame.image.load('cat.png')
 mouseImg = pygame.image.load('mouse.png')
 cheeseImg = pygame.image.load('cheese.png')
+cScoreImg = pygame.image.load('cScore.png')
+mScoreImg = pygame.image.load('mScore.png')
 
 white = (255, 255, 255)
 black = (0, 0, 0)
@@ -22,6 +24,10 @@ mouse_width = 38
 mouse_height = 25
 cheese_width = 35
 cheese_height = 34
+cScore_width = 206
+cScore_height = 29
+mScore_width = 206
+mScore_height = 29
 
 #cat starting point info
 x = (50)
@@ -35,6 +41,9 @@ mouseRect = pygame.Rect(mx, my, mouse_width, mouse_height)
 cx = 0
 cy = display_height - cheese_height
 cheeseRect = pygame.Rect(cx, cy, cheese_width, cheese_height)
+#score location
+sx = 0
+sy = 0
 
 #cat location change variables
 x_change = 0
@@ -65,8 +74,7 @@ def cheese(cx,cy):
 #game loop
 game_over = False
 while game_over == False:
-    catRect = pygame.Rect(x, y, cat_width, cat_height)
-    mouseRect = pygame.Rect(mx, my, mouse_width, mouse_height)
+    
 #set up quit when user clicks the 'x'
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -125,36 +133,39 @@ while game_over == False:
 #set up x & y change of cat to stay within window boundaries
     
 
-    if not catRect.colliderect(mouseRect):
-        new_x = x + x_change
-        new_y = y + y_change
-        if new_x >=0 and new_x <= display_width - cat_width:
-            x += x_change
+    #if not catRect.colliderect(mouseRect):
+    new_x = x + x_change
+    new_y = y + y_change
+    if new_x >=0 and new_x <= display_width - cat_width:
+        x += x_change
 
-                                
-        if new_y >= 0 and new_y <= display_height - cat_height:
-            y += y_change
+                                    
+    if new_y >= 0 and new_y <= display_height - cat_height:
+        y += y_change
     
 
 #set up x & y change of mouse to stay within window boundaries
-        new_mx = mx + mx_change
-        new_my = my + my_change
+    new_mx = mx + mx_change
+    new_my = my + my_change
         
-        if new_mx >=0 and new_mx <= display_width - mouse_width:
-            mx += mx_change
+    if new_mx >=0 and new_mx <= display_width - mouse_width:
+        mx += mx_change
 
             
-        if new_my >= 0 and new_my <= display_height - mouse_height:
-            my += my_change
+    if new_my >= 0 and new_my <= display_height - mouse_height:
+        my += my_change
 
-    else:
-        x -= x_change
-        y -= y_change
 
 #set up when cat touches mouse sensing
     #if catRect.colliderect(mouseRect):
-        
-
+    catRect = pygame.Rect(x, y, cat_width, cat_height)
+    mouseRect = pygame.Rect(mx, my, mouse_width, mouse_height)
+    
+    if catRect.colliderect(mouseRect):
+        x -= x_change
+        y -= y_change
+        mx -= mx_change
+        my -= my_change
     
 
 
@@ -162,6 +173,8 @@ while game_over == False:
 
 #draw the frame
     gameDisplay.fill(white)
+    gameDisplay.blit(cScoreImg, (sx,sy))
+    gameDisplay.blit(mScoreImg, (0, cScore_height + 10))
     cat(x,y)
     mouse(mx,my)
     cheese(cx,cy)
